847a3191b5afe928bd48b1e9b4feb6c334877f6c
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-05-26 09:39:42 +0300

    retrieve categories

--- src/pages/RemoveStock.jsx
+++ src/pages/RemoveStock.jsx
@@ -1,13 +1,27 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import Category from "../backend/entities/Category.js";
 
 function RemoveStock() {
     const navigate = useNavigate()
+    const [categories, setCategories] = useState([])
 
     useEffect(() => {
+
         const password = prompt("Entrez mot de passe: ")
         if (password === "admin123") {
-            console.log("tafiditra")
+            const loadCategories = async () => {
+                try {
+                    const categoryApi = new Category({}, false);
+                    const categoryList = await categoryApi.getExcl([1, 2]);
+
+                    setCategories(categoryList);
+                } catch (error) {
+                    console.error("Error fetching categories:", error);
+                }
+            };
+
+            loadCategories()
         } else {
             alert("Erreur")
             navigate("/fo/products")
@@ -15,7 +29,9 @@ function RemoveStock() {
     }, []);
 
 
-    return <h1>Hello</h1>
+    return <div>
+
+    </div>
 }
 
 export default RemoveStock;
