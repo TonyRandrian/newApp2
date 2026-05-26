03009809d6e49c0a514cb15bf4231dd79641c119
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-05-26 09:47:29 +0300

    get props

--- src/pages/RemoveStock.jsx
+++ src/pages/RemoveStock.jsx
@@ -5,6 +5,8 @@ import Category from "../backend/entities/Category.js";
 function RemoveStock() {
     const navigate = useNavigate()
     const [categories, setCategories] = useState([])
+    const [categoryId, setCategoryId] = useState("");
+    const [qtt, setQtt] = useState("")
 
     useEffect(() => {
 
@@ -28,9 +30,34 @@ function RemoveStock() {
         }
     }, []);
 
+    const removeStock = () => {
+        console.log(qtt + " skfjs " + categoryId)
+    }
 
     return <div>
-
+        <div className="fo-filter">
+            <label className="fo-filter__label">Catégorie</label>
+            <select
+                value={categoryId}
+                onChange={(e) => setCategoryId(e.target.value)}
+            >
+                {categories.map((category, index) => (
+                    <option key={`${category.id}-${index}`} value={category.id}>
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <div>
+            QUANTITE
+            <input
+                type={"number"}
+                onChange={(e) => setQtt(e.target.value)}
+            />
+        </div>
+        <div>
+            <button onClick={removeStock}>Valider</button>
+        </div>
     </div>
 }
 
